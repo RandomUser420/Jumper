97920bbe0a06ec18a6f96cf5481f30da22a58d32
RandomUser420 <56744545+RandomUser420@users.noreply.github.com>
2020-02-28 14:19:13 -1000

    Add files via upload

---
+++ jumper.js
@@ -0,0 +1,60 @@
+function jumper(){
+  this.x=0;
+  this.y=0;
+  this.gravity=0.5; // force of gravity
+  this.lift=-10; // opposing force of gravity
+  this.velocity=0; // speed of gravity force
+  
+  
+  //I am building a function to display on the screen. This is where I put my values of what the jumper will look like
+  this.show=function(){
+    fill(255,0,0);
+    ellipse(this.x,this.y,50,50);
+    
+  };
+  
+  //build a function called "UP" that will take the initial velocity and modify by opposing gravity
+  
+  this.up= function(){
+    
+      this.velocity += this.lift;
+      
+    
+  };
+  
+ 
+ //This will continuosly update jumper
+ 
+ 
+  this.update = function(){
+    
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+    this.velocity*=0.9; // Air resistance
+    
+    //this will prevent the jumper from leaving the ground
+    if(this.y>h){
+      this.y = h;
+      this.velocity=0;
+    }
+    // this will prevent the jumper from leaving the ceiling
+    
+    if(this.y<0){
+      this.y=0;
+      this.velocity=0;
+    }
+    
+    
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+}
